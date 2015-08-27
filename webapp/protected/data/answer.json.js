7e4786a6f71114aa4c39ac4f853c28d1966ca887
nicolas <nicolas@10.112.60.87>
2015-08-27 16:22:45 +0200

    init projet à partir de quality forms

---
+++ webapp/protected/data/answer.json.js
@@ -0,0 +1,14 @@
+db.questionnaire_answer.insert({
+    "questionnaireid":"depositform",
+    "userid":"",
+    "answers":
+            [{
+                    "groupidquestionid":"g1q1",
+                    "answer": "myanswer",
+                },
+                {
+                    "groupidquestionid": "g1q2",
+                    "answer": "thanks"
+                }]
+
+})
---
+++ webapp/protected/data/users.json.js
@@ -0,0 +1,9 @@
+db.user.insert({
+    "login":"nicolas",
+    "password":"nicolas",
+});
+
+db.user.insert({
+    "login":"maha",
+    "password":"maha",
+});
