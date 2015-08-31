b9a73eb7a0682d61bb9416d21d72a72143ec3740
mpenicaud <matthieu.penicaud@inserm.fr>
2015-08-31 17:23:04 +0200

    partial commit : prepare web service consumtion.

--- webapp/protected/models/Patient.php
+++ webapp/protected/models/Patient.php
@@ -3,64 +3,48 @@
 /**
  * This is the MongoDB Document model class based on table "patient".
  */
-class Patient extends EMongoDocument
+class Patient extends CFormModel
 {
-	public $nom;
-	public $prenom;
-	public $date_naissance;
-	public $nom_naissance;
+    public $nom;
+    public $prenom;
+    public $date_naissance;
+    public $nom_naissance;
+    public $sexe;
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return Patient the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @return Patient the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * returns the primary key field for this model
-	 */
-	public function primaryKey()
-	{
-		return NULL;
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('nom, prenom, date_naissance, nom_naissance', 'required'),
+            array('nom, prenom, nom_naissance', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('nom, prenom, date_naissance, nom_naissance', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return string the associated collection name
-	 */
-	public function getCollectionName()
-	{
-		return 'patient';
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'nom' => 'Nom',
+            'prenom' => 'Prenom',
+            'date_naissance' => 'Date Naissance',
+            'nom_naissance' => 'Nom Naissance',
+            'sexe' => 'Sexe',
+        );
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('nom, prenom, date_naissance, nom_naissance', 'required'),
-			array('nom, prenom, nom_naissance', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('nom, prenom, date_naissance, nom_naissance', 'safe', 'on'=>'search'),
-		);
-	}
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'nom' => 'Nom',
-			'prenom' => 'Prenom',
-			'date_naissance' => 'Date Naissance',
-			'nom_naissance' => 'Nom Naissance',
-		);
-	}
 }
--- webapp/protected/views/site/patient.php
+++ webapp/protected/views/site/patient.php
@@ -8,49 +8,57 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'patient-patient-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// See class documentation of CActiveForm for details on this,
-	// you need to use the performAjaxValidation()-method described there.
-        'action'=>Yii::app()->createUrl('site/affichepatient'),
-	'enableAjaxValidation'=>false,
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'patient-patient-form',
+        // Please note: When you enable ajax validation, make sure the corresponding
+        // controller action is handling ajax validation correctly.
+        // See class documentation of CActiveForm for details on this,
+        // you need to use the performAjaxValidation()-method described there.
+        // 'action'=>Yii::app()->createUrl('site/affichepatient'),
+        'enableAjaxValidation' => false,
+    ));
+    ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
+    <?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'nom'); ?>
-		<?php echo $form->textField($model,'nom'); ?>
-		<?php echo $form->error($model,'nom'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'nom'); ?>
+        <?php echo $form->textField($model, 'nom'); ?>
+        <?php echo $form->error($model, 'nom'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'prenom'); ?>
-		<?php echo $form->textField($model,'prenom'); ?>
-		<?php echo $form->error($model,'prenom'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'prenom'); ?>
+        <?php echo $form->textField($model, 'prenom'); ?>
+        <?php echo $form->error($model, 'prenom'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'date_naissance'); ?>
-		<?php echo $form->textField($model,'date_naissance'); ?>
-		<?php echo $form->error($model,'date_naissance'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'date_naissance'); ?>
+        <?php echo $form->textField($model, 'date_naissance'); ?>
+        <?php echo $form->error($model, 'date_naissance'); ?>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'nom_naissance'); ?>
-		<?php echo $form->textField($model,'nom_naissance'); ?>
-		<?php echo $form->error($model,'nom_naissance'); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->labelEx($model, 'nom_naissance'); ?>
+        <?php echo $form->textField($model, 'nom_naissance'); ?>
+        <?php echo $form->error($model, 'nom_naissance'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'sexe'); ?>
+        <?php echo $form->dropDownList($model, 'sexe', array('M' => 'Masculin', 'F' => 'Féminin')); ?>
+        <?php echo $form->error($model, 'sexe'); ?>
+    </div>
 
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
-	</div>
+    <div class="row buttons">
+        <?php echo CHtml::submitButton('Submit'); ?>
+    </div>
 
-<?php $this->endWidget(); ?>
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
