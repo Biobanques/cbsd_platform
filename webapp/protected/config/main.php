8d5cbee85b47fb15f07802e40d1c5cf932e4972a
te <te@te-OptiPlex-960>
2015-09-02 16:26:37 +0200

    Maj resetdb script

--- webapp/protected/config/main.php
+++ webapp/protected/config/main.php
@@ -32,7 +32,7 @@ return array(
     'modules' => array(
         'gii' => array(
             'class' => 'system.gii.GiiModule',
-            'password' => 'sakuragigou',
+            'password' => 'password',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters' => array('127.0.0.1', '::1'),
             'generatorPaths' => array('ext.YiiMongoDbSuite.gii'),
--- webapp/protected/data/resetdb_bernard.sh
+++ webapp/protected/data/resetdb_bernard.sh
@@ -6,9 +6,13 @@
 # @version 1.0 
 ####################################################################
 
+# test existing cbsd_platform project into /var/www/html/, if not, clone the project from GitHub
+
 if [ ! -d "/var/www/html/cbsd_platform/" ];then
     git clone https://github.com/Biobanques/cbsd_platform.git
 fi
+
+# create folders "assets" and "runtime" and give the user write access to theses folders 
 
 chmod -R ugo+rwx /var/www/html/cbsd_platform/
 mkdir /var/www/html/cbsd_platform/webapp/assets
@@ -16,10 +20,14 @@ chmod -R ugo+rwx /var/www/html/cbsd_platform/webapp/assets
 mkdir /var/www/html/cbsd_platform/webapp/protected/runtime
 chmod -R ugo+rwx /var/www/html/cbsd_platform/webapp/protected/runtime
 
+# copy a new CommonProperties and set database
+
 cp /var/www/html/cbsd_platform/webapp/protected/CommonProperties_default_1_1.php /var/www/html/cbsd_platform/webapp/protected/CommonProperties.php
 
-MESSAGE="'mongodb:\/\/admin_cbsd:cbsd2015@localhost\/cbsdforms'"
-sed -i -e "s/'mongodb:\/\/qfuseradmin:bbanques2015@localhost\/qualityformsdb'/$MESSAGE/g" /var/www/html/cbsd_platform/webapp/protected/CommonProperties.php
+DATABASE="'mongodb:\/\/admin_cbsd:cbsd2015@localhost\/cbsdforms'"
+sed -i -e "s/'mongodb:\/\/qfuseradmin:bbanques2015@localhost\/qualityformsdb'/$DATABASE/g" /var/www/html/cbsd_platform/webapp/protected/CommonProperties.php
+
+# import "questionnaire", "answer" and "users"
 
 mongo cbsdforms --port 27017 -u admin_cbsd -p 'cbsd2015' ./parkinson.json.js
 mongo cbsdforms --port 27017 -u admin_cbsd -p 'cbsd2015' ./questionnaire.json.js
