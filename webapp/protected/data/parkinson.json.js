dfe6fcb43891167690a0069706e581ac1e0493d2
te <te@te-OptiPlex-960>
2015-09-07 17:06:04 +0200

    maj Formulaire Parkinson

--- webapp/protected/data/parkinson.json.js
+++ webapp/protected/data/parkinson.json.js
@@ -3,7 +3,7 @@ db.questionnaire.drop()
 db.questionnaire.insert({
     "id": "parkinsonform",
     "name": "Parkinson form",
-    "name_fr":"Formulaire Parkinson",
+    "name_fr": "Formulaire Parkinson",
     "description": "Formulaire Parkinson",
     "last_modified": new Date(),
     "message_start": "Welcome to the deposit form for DNA/RNA, cells, fluids and tissues<br />(*)1 form per patient and per sampling date<br /> (*) 1 fiche par patient et par date de prélèvement",
@@ -48,7 +48,7 @@ db.questionnaire.insert({
                             "label_fr": "Lieu de naissance",
                             "type": "input",
                             "order": "4",
-			    "style": "float:right"
+                            "style": "float:right"
                         },
                         {
                             "id": "place",
@@ -78,7 +78,7 @@ db.questionnaire.insert({
                             "label": "",
                             "label_fr": "",
                             "type": "radio",
-			    "values": "Maladie,Retraite,Retraite anticipé",
+                            "values": "Maladie,Retraite,Retraite anticipé",
                             "order": "8"
                         },
                         {
@@ -86,10 +86,361 @@ db.questionnaire.insert({
                             "label": "",
                             "label_fr": "",
                             "type": "radio",
-			    "values": "Maladie,Autres,Non déterminé",
+                            "values": "Maladie,Autres,Non déterminé",
                             "order": "9"
+                        },
+                        {
+                            "id": "trouble_memoire",
+                            "label": "",
+                            "label_fr": "Trouble de la mémoire épisodique",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "10"
+                        },
+                        {
+                            "id": "life",
+                            "label": "",
+                            "label_fr": "Entravant la vie quotidienne",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "11"
+                        },
+                        {
+                            "id": "six_months",
+                            "label": "",
+                            "label_fr": "Depuis plus de 6 mois",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "12"
+                        },
+                        {
+                            "id": "trouble_comportement",
+                            "label": "",
+                            "label_fr": "Troubles du comportement",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "13"
+                        },
+                        {
+                            "id": "aphasie",
+                            "label": "",
+                            "label_fr": "Aphasie",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "14"
+                        },
+                        {
+                            "id": "apraxie",
+                            "label": "",
+                            "label_fr": "Apraxie",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "15"
+                        },
+                        {
+                            "id": "agnosie_visuelle",
+                            "label": "",
+                            "label_fr": "Agnosie visuelle",
+                            "type": "radio",
+                            "values": "Oui,Non",
+                            "order": "16"
                         }
                     ]
-             }]
+                },
+                {
+                    "id": "adl",
+                    "title": "",
+                    "title_fr": "ACTIVITES DE LA VIE QUOTIDIENNE (ADL)",
+                    "parent_group": "parkinsonform",
+                    "questions": [{
+                            "id": "adl1",
+                            "label": "",
+                            "label_fr": "Ne reçoit aucune aide (rentre et sort seul de la baignoire si celle-ci est le moyen habituel de toilette.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "17"
+                        },
+                        {
+                            "id": "adl2",
+                            "label": "",
+                            "label_fr": "Reçoit de l'aide pour laver certaines parties du corps (comme le dos ou une jambe).",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "18"
+                        },
+                        {
+                            "id": "adl3",
+                            "label": "",
+                            "label_fr": "Reçoit de 1'aide pour laver plus d'une partie du corps.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "19"
+                        },
+                        {
+                            "id": "adl4",
+                            "label": "",
+                            "label_fr": "Prend les vêtements et s'habille complètement sans aide.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "20"
+                        },
+                        {
+                            "id": "adl5",
+                            "label": "",
+                            "label_fr": "Prend les habits et s'habille sans aide sauf pour les chaussures.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "21"
+                        },
+                        {
+                            "id": "adl6",
+                            "label": "",
+                            "label_fr": "Reçoit de l'aide pour prendre les habits et/ou s'habiller ou reste partiellement ou totalement dévêtu.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "22"
+                        },
+                        {
+                            "id": "adl7",
+                            "label": "",
+                            "label_fr": "Va aux toilettes, se nettoie et arrange ses vêtements sans aide (peut s'aider d'un support comme une canne, un déambulateur, une chaise roulante et peut utiliser un bassin ou une chaise percée).",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "23"
+                        },
+                        {
+                            "id": "adl8",
+                            "label": "",
+                            "label_fr": "Reçoit de l'aide pour aller aux toilettes... ne va pas aux toilettes.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "24"
+                        },
+                        {
+                            "id": "adl9",
+                            "label": "",
+                            "label_fr": "Se couche et se lève du lit aussi bien qu'il s'assoit ou se lève d'une chaise, sans aide (peut s'aider d'un support comme un déambulateur ou une canne).",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "25"
+                        },
+                        {
+                            "id": "adl10",
+                            "label": "",
+                            "label_fr": "Se couche ou se lève avec aide.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "26"
+                        },
+                        {
+                            "id": "adl11",
+                            "label": "",
+                            "label_fr": "Reste alité.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "27"
+                        },
+                        {
+                            "id": "adl12",
+                            "label": "",
+                            "label_fr": "Contrôle parfaitement seul son élimination.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "28"
+                        },
+                        {
+                            "id": "adl13",
+                            "label": "",
+                            "label_fr": "A quelques petits accidents ou est incotinent (urine ou selles).",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "29"
+                        },
+                        {
+                            "id": "adl14",
+                            "label": "",
+                            "label_fr": "Mange sans aide.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "30"
+                        },
+                        {
+                            "id": "adl15",
+                            "label": "",
+                            "label_fr": "Mange seul mais a besoin d'une aide pour couper la viande ou pour beurrer les tartines.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "31"
+                        },
+                        {
+                            "id": "adl16",
+                            "label": "",
+                            "label_fr": "Reçoit une aide pour manger ou et nourrit partiellement ou totalement à l’aide d'une sonde ou de solutés intraveineux.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "32"
+                        },
+  
+                    ]
+                },
+                {
+                    "id": "iadl",
+                    "title": "",
+                    "title_fr": "IADL",
+                    "parent_group": "parkinsonform",
+                    "questions": [{
+                            "id": "iadl1",
+                            "label": "",
+                            "label_fr": "Se sert normalement du téléphone.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "33"
+                        },
+                        {
+                            "id": "iadl2",
+                            "label": "",
+                            "label_fr": "Compose quelques numéros très connus.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "34"
+                        },
+                        {
+                            "id": "iadl3",
+                            "label": "",
+                            "label_fr": "N’utilise pas du tout le téléphone spontanément.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "35"
+                        },
+                        {
+                            "id": "iadl4",
+                            "label": "",
+                            "label_fr": "Incapable d’utiliser le téléphone.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "36"
+                        },
+                        {
+                            "id": "iadl5",
+                            "label": "",
+                            "label_fr": "Utilise les moyens de transports de façon indépendante ou conduit sa propre voiture.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "37"
+                        },
+                        {
+                            "id": "iadl6",
+                            "label": "",
+                            "label_fr": "Organise ses déplacements en taxi ou n’utilise aucun moyen de transport public.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "38"
+                        },
+                        {
+                            "id": "iadl7",
+                            "label": "",
+                            "label_fr": "Utilise les transports publics avec l’aide de quelqu’un.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "39"
+                        },
+                        {
+                            "id": "iadl8",
+                            "label": "",
+                            "label_fr": "Déplacement limité en taxi ou en voiture avec l’aide de quelqu’un.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "40"
+                        },
+                        {
+                            "id": "iadl9",
+                            "label": "",
+                            "label_fr": "Est responsable de la prise de ses médicaments (dose et rythmes corrects).",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "41"
+                        },
+                        {
+                            "id": "iadl10",
+                            "label": "",
+                            "label_fr": "Est responsable de la prise de ses médicaments si les doses ont été préparées à l’avance.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "42"
+                        },
+                        {
+                            "id": "iadl11",
+                            "label": "",
+                            "label_fr": "Est incapable de prendre seul ses médicaments même si ceux-ci ont été à l’avance.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "43"
+                        },
+                        {
+                            "id": "iadl12",
+                            "label": "",
+                            "label_fr": "Non applicable, n’a jamais manipulé l’argent.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "44"
+                        },
+                        {
+                            "id": "iadl13",
+                            "label": "",
+                            "label_fr": "Gère ses finances de façon autonome.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "45"
+                        },
+                        {
+                            "id": "iadl14",
+                            "label": "",
+                            "label_fr": "Se débrouille pour les achats quotidiens, mais a besoin d’aide pour les opérations à la banque et les achats importants.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "46"
+                        },
+                        {
+                            "id": "iadl15",
+                            "label": "",
+                            "label_fr": "Incapable de manipuler l’argent.",
+                            "type": "list",
+                            "values": "1,2,3",
+                            "values_fr": "1,2,3",
+                            "order": "47"
+                        }
+                    ]
+                }
+            ]
 
 })
