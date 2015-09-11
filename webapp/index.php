b492d27c732563b25719e2c6a770420b8eaadc2a
te <te@te-OptiPlex-960>
2015-09-11 10:17:30 +0200

    upgrade framework yii 1.1.15 > yii 1.1.16

--- webapp/index.php
+++ webapp/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/yii-1.1.15/framework/yii.php';
+$yii=dirname(__FILE__).'/yii-1.1.16.bca042/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 include dirname(__FILE__) . '/protected/components/CommonProperties.php';
