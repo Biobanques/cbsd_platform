790c653876060464d7134d9b5529c9c9e9fd39b9
te <te@te-OptiPlex-960>
2015-09-28 14:12:43 +0200

    fix régression user-edit

--- webapp/protected/data/users.json.js
+++ webapp/protected/data/users.json.js
@@ -1,11 +1,11 @@
 db.user.drop();
 db.user.insert({
     "login":"bernard",
-    "password":"bernard",
+    "password":"bernard2015",
     "profil":"1",
     "nom":"te",
     "prenom":"bernard",
-    "email":"bernardte90@gmail.com",
+    "email":"bernard.te@inserm.fr",
     "telephone":"0625004713",
     "gsm":"",
     "inactif":"0"
@@ -13,68 +13,63 @@ db.user.insert({
 
 db.user.insert({
     "login":"nicolas",
-    "password":"nicolas",
+    "password":"nicolas2015",
     "profil":"1",
     "nom":"malservet",
     "prenom":"nicolas",
-    "email":"nicolas@nicolas.com",
-    "telephone":"0625004713",
+    "email":"nicolas.malservet@inserm.fr",
+    "telephone":"0123456789",
+    "gsm":"",
+    "inactif":"0"
+});
+
+db.user.insert( {
+   
+    "login" : "matth",
+    "password" : "guizmo2015",
+    "profil":"1",
+    "nom":"penicaud",
+    "prenom":"matthieu",
+    "email":"matthieu.penicaud@inserm.fr",
+    "telephone":"0123456789",
     "gsm":"",
     "inactif":"0"
 });
 
 db.user.insert({
     "login":"clinicien",
-    "password":"clinicien",
+    "password":"clinicien2015",
     "profil":"0",
     "nom":"clinicien",
     "prenom":"clinicien",
     "email":"clinicien@clinicien.com",
     "telephone":"0123456789",
     "gsm":"",
-    "inactif":"0"
+    "inactif":"0",
+    "address":"24 rue de Paris"
 });
 
 db.user.insert({
-    "login":"neuropathologiste",
-    "password":"neuropathologiste",
+    "login":"neuropath",
+    "password":"neuropath2015",
     "profil":"2",
-    "nom":"neuropathologiste",
-    "prenom":"neuropathologiste",
-    "email":"neuropathologiste@neuropathologiste.com",
+    "nom":"neuropath",
+    "prenom":"neuropath",
+    "email":"neuropath@neuropath.com",
     "telephone":"0123456789",
     "gsm":"",
-    "inactif":"1"
+    "inactif":"1",
+    "centre":"CHU"
 });
 
 db.user.insert({
-    "login":"généticien",
-    "password":"généticien",
+    "login":"geneticien",
+    "password":"geneticien2015",
     "profil":"3",
-    "nom":"généticien",
-    "prenom":"généticien",
-    "email":"généticien@généticien.com",
+    "nom":"geneticien",
+    "prenom":"geneticien",
+    "email":"geneticien@geneticien.com",
     "telephone":"0123456789",
     "gsm":"",
     "inactif":"1"
-});
-
-db.user.insert({
-    "login":"invité",
-    "password":"invité",
-    "profil":"0",
-    "nom":"invité",
-    "prenom":"invité",
-    "email":"",
-    "telephone":"",
-    "gsm":"",
-    "inactif":"1"
-});
-
-   db.user.insert( {
-   
-    "login" : "matth",
-    "password" : "guizmo",
-    "role" : "admin"
-
 });
