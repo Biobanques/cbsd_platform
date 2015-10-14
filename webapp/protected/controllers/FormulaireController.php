9a264d7593cf7c49385afb70562b223da4b36a98
te <te@te-OptiPlex-960>
2015-10-14 10:43:04 +0200

    debug Formulaire

--- webapp/protected/controllers/FormulaireController.php
+++ webapp/protected/controllers/FormulaireController.php
@@ -19,14 +19,13 @@ class FormulaireController extends Controller
     public function filters() {
         return array(
             'accessControl', // perform access control for CRUD operations
-            'postOnly + delete', // we only allow deletion via POST request
         );
     }
     
     public function accessRules() {
         return array(
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => array('create', 'update', 'admin', 'view', 'delete'),
+                'actions' => array('create', 'update', 'admin', 'view', 'delete', 'deleteQuestion', 'deleteQuestionGroup', 'dynamicquestions'),
                 'expression' => '$user->isAdmin()'
             ),
             array('deny', // deny all users
