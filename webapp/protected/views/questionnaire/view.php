78a72aca8c778c176043be6e835b3cd0820cd12f
bernardte <bernardte@ubuntu>
2015-11-02 10:22:05 +0100

    issue #8: hide contributors buttons

--- webapp/protected/views/questionnaire/view.php
+++ webapp/protected/views/questionnaire/view.php
@@ -44,40 +44,7 @@ echo CHtml::link('Vue une page HTML', array('questionnaire/viewOnePage', 'id' =>
 $img = CHtml::image(Yii::app()->request->baseUrl . '/images/page_white_acrobat.png', 'export as pdf');
 echo CHtml::link($img, array('questionnaire/exportPDF', 'id' => $model->_id), array());
 ?>
-<br><bR>
-<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id' => 'myModalContributors')); ?>
 
-<div class="modal-header">
-    <a class="close" data-dismiss="modal">&times;</a>
-    <h4>Contributors</h4>
-</div>
-
-<div class="modal-body span5" >
-    <?php echo $model->renderContributors(); ?>
-</div>
-
-<div class="modal-footer">
-    <?php
-    $this->widget('bootstrap.widgets.TbButton', array(
-        'label' => 'Close',
-        'url' => '#',
-        'htmlOptions' => array('data-dismiss' => 'modal'),
-    ));
-    ?>
-</div>
-
-<?php $this->endWidget(); ?>
-
-<?php
-$this->widget('bootstrap.widgets.TbButton', array(
-    'label' => 'Contributors',
-    'type' => 'primary',
-    'htmlOptions' => array(
-        'data-toggle' => 'modal',
-        'data-target' => '#myModalContributors',
-    ),
-));
-?>
 <?php echo CHtml::errorSummary($model, null, null, array('class' => 'alert alert-error')); ?>
 <div>
     <?php
