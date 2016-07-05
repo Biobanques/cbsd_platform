bc63b18b48cca9c02ffb6ba831cea2b3e000661f
mpenicaud <matthieu.penicaud@inserm.fr>
2016-07-05 11:29:03 +0200

    phpunit5.4

--- webapp/protected/tests/bootstrap.php
+++ webapp/protected/tests/bootstrap.php
@@ -3,11 +3,11 @@
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../yii-1.1.17.467ff50/framework/yiit.php';
-$config=dirname(__FILE__).'/../config/test.php';
+$yiit = dirname(__FILE__) . '/../../yii-1.1.17.467ff50/framework/yiit.php';
+$config = dirname(__FILE__) . '/../config/test.php';
 include dirname(__FILE__) . '/../../protected/components/CommonProperties.php';
 
 require_once($yiit);
-require_once(dirname(__FILE__).'/WebTestCase.php');
+
 
 Yii::createWebApplication($config);
