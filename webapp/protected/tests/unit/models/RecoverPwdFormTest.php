9c9be5cd853d72c34c9a238fcf2bfcec410ff80b
te <te@te-OptiPlex-9020>
2016-07-22 16:49:06 +0200

    delete unusued model

--- webapp/protected/tests/unit/models/RecoverPwdFormTest.php
+++ webapp/protected/tests/unit/models/RecoverPwdFormTest.php
@@ -23,4 +23,33 @@ class RecoverPwdFormTest extends PHPUnit_Framework_TestCase {
         $this->assertInternalType('array', $model->validateFields());
     }
 
+    /**
+     * testing method
+     */
+    public function testValidateFields() {
+        $model = new RecoverPwdForm;
+        $this->assertEmpty($model->login);
+        $this->assertEmpty($model->email);
+        $model->login = "Test";
+        $model->email = "test@test.fr";
+        $this->assertNotEmpty($model->login);
+        $this->assertNotEmpty($model->email);
+        $user = $this->getUser();
+        $this->assertNotNull($user);
+    }
+    
+    public function getUser() {
+        $model = new User;
+        $model->login = "test";
+        $model->password = "test2016";
+        $model->profil = "clinicien";
+        $model->nom = "testNom";
+        $model->prenom = "testPrénom";
+        $model->email = "test@gmail.com";
+        $model->telephone = "0123456789";
+        $model->address = "5 rue Marat";
+        $model->centre = "Paris";
+        
+        return $model;
+    }
 }
