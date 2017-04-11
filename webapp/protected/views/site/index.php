400069283260100e4c72fc1a55e00985067b345e
te <te@te-OptiPlex-9020>
2017-04-11 17:19:12 +0200

    fix datepicker for firefox

--- webapp/protected/views/site/index.php
+++ webapp/protected/views/site/index.php
@@ -1,19 +1,6 @@
 <?php
 /* @var $this SiteController */
 $this->pageTitle = Yii::app()->name;
-echo $_SESSION["patientBirthDate"] . "<br>";
-if (strpos($_SESSION["patientBirthDate"], '/')) {
-                    $birthdateFormat = explode('/', $_SESSION["patientBirthDate"]);
-                } else {
-                    $birthdateFormat = explode('-', $_SESSION["patientBirthDate"]);
-                }
-                var_dump($birthdateFormat);
-                $dateNow = explode('/', date(CommonTools::FRENCH_SHORT_DATE_FORMAT));
-                if (($birthdateFormat[1] < $dateNow[1]) || (($birthdateFormat[1] == $dateNow[1]) && ($birthdateFormat[0] <= $dateNow[0]))) {
-                    $valueInput = $dateNow[2] - $birthdateFormat[2];
-                } else {
-                    $valueInput = $dateNow[2] - $birthdateFormat[2] - 1;
-                }
 ?>
 
 <div class="jumbotron">
